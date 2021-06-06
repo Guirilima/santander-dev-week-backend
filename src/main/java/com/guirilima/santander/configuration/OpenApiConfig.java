c3af5f6af1a5fe4b17343594995eab24ba8d6c65
Guirilima <ungui@outlook.com.br>
2021-06-06 00:23:59 -0300

    Adding OpenAPi config and getting project info

---
+++ src/main/java/com/guirilima/santander/configuration/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.guirilima.santander.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private final BuildProperties buildProperties;
+
+    @Autowired
+    public OpenApiConfig (BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+                new Info()
+                    .title(buildProperties.getName() + " - Guirilima")
+                    .version(buildProperties.getVersion())
+                    .termsOfService("http://swagger.io/terms")
+                    .license(
+                            new License()
+                                    .name("Apache 2.0")
+                                    .url("http://springdoc.org")));
+    }
+}
